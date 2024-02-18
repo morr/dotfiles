b207abdd9c69632d093bc9ef16899877e7efce2b
Andrey Sidorov <takandar@gmail.com>
2024-02-19 02:00:47 +0300

    nvim config

---
+++ nvim/lua/plugins/workflow.lua
@@ -0,0 +1,12 @@
+return {
+   {
+      "tpope/vim-speeddating",
+   },
+   {
+      "fedepujol/move.nvim",
+      keys = {
+         { "K", ":MoveBlock(-1)<CR>", mode = "v" },
+         { "J", ":MoveBlock(1)<CR>", mode = "v" },
+      },
+   },
+}
