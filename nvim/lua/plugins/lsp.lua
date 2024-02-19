09d4cb9f19d2f8dd5cede4b0be64b9fc92d26620
Andrey Sidorov <takandar@gmail.com>
2024-02-20 02:29:24 +0300

    basic completion now works

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -43,59 +43,59 @@ local languages = {
 }
 
 return {
-   {
-      "neovim/nvim-lspconfig",
-      dependencies = { "hrsh7th/cmp-nvim-lsp" },
-      config = function()
-         local capabilities = require("cmp_nvim_lsp").default_capabilities()
-         require("lspconfig").lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-               Lua = {
-                  diagnostics = {
-                     globals = { "vim", "describe", "it" },
-                  },
-               },
-            },
-         })
-   
-         for _, language in pairs(languages) do
-            require("lspconfig")[language].setup({
-               capabilities = capabilities,
-            })
-         end
-   
-         vim.keymap.set(
-            "n",
-            "<Leader>fa",
-            ":EslintFixAll<CR>",
-            { noremap = true, silent = true }
-         )
-   
-         vim.lsp.handlers["textDocument/publishDiagnostics"] =
-            vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-               virtual_text = false,
-            })
-      end,
-   },
-   {
-      "williamboman/mason.nvim",
-      opts = {},
-   },
-   {
-      "williamboman/mason-lspconfig.nvim",
-      opts = {
-         ensure_installed = {
-            "lua_ls",
-            -- "clangd",
-            "html",
-            "cssls",
-            -- "tsserver",
-            -- "eslint",
-            -- "tailwindcss",
-            -- "pyright",
-            -- "gopls",
-         },
-      },
-   },
+   -- {
+   --    "neovim/nvim-lspconfig",
+   --    dependencies = { "hrsh7th/cmp-nvim-lsp" },
+   --    config = function()
+   --       local capabilities = require("cmp_nvim_lsp").default_capabilities()
+   --       require("lspconfig").lua_ls.setup({
+   --          capabilities = capabilities,
+   --          settings = {
+   --             Lua = {
+   --                diagnostics = {
+   --                   globals = { "vim", "describe", "it" },
+   --                },
+   --             },
+   --          },
+   --       })
+   --
+   --       for _, language in pairs(languages) do
+   --          require("lspconfig")[language].setup({
+   --             capabilities = capabilities,
+   --          })
+   --       end
+   --
+   --       vim.keymap.set(
+   --          "n",
+   --          "<Leader>fa",
+   --          ":EslintFixAll<CR>",
+   --          { noremap = true, silent = true }
+   --       )
+   --
+   --       vim.lsp.handlers["textDocument/publishDiagnostics"] =
+   --          vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+   --             virtual_text = false,
+   --          })
+   --    end,
+   -- },
+   -- {
+   --    "williamboman/mason.nvim",
+   --    opts = {},
+   -- },
+   -- {
+   --    "williamboman/mason-lspconfig.nvim",
+   --    opts = {
+   --       ensure_installed = {
+   --          "lua_ls",
+   --          -- "clangd",
+   --          "html",
+   --          "cssls",
+   --          -- "tsserver",
+   --          -- "eslint",
+   --          -- "tailwindcss",
+   --          -- "pyright",
+   --          -- "gopls",
+   --       },
+   --    },
+   -- },
 }
