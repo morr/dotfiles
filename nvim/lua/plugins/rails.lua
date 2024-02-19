352427fc607ffecdc9b54f6a2f0eeba0ef85b788
Andrey Sidorov <takandar@gmail.com>
2024-02-19 15:59:21 +0300

    add rails support

---
+++ nvim/lua/plugins/rails.lua
@@ -0,0 +1,27 @@
+return {
+   {
+      'tpope/vim-rails',
+      init = function()
+         vim.keymap.set("n", "<leader>s", '<cmd>AV<cr>', { desc = "Open spec in split view" })
+         vim.keymap.set("n", "<leader>S", '<cmd>A<cr>', { desc = "Open spec in current buffer" })
+         -- example projections https://gist.github.com/henrik/5676109
+         vim.g.rails_projections = {
+            ['app/*.rb'] = {
+               alternate = 'spec/{}_spec.rb',
+            },
+            ['app/admin/*.rb'] = {
+               alternate = 'spec/controllers/admin/{}_controller_spec.rb',
+            },
+            ['spec/controllers/admin/*_controller_spec.rb'] = {
+               alternate = 'app/admin/{}.rb',
+            },
+            ['config/locales/*ru.yml'] = {
+               alternate = 'config/locales/{}en.yml',
+            },
+            ['config/locales/*en.yml'] = {
+               alternate = 'config/locales/{}ru.yml',
+            }
+         }
+      end
+   }
+}
