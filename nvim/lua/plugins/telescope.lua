5332fce822c9792e972c99468f713309222d0726
Andrey Sidorov <takandar@gmail.com>
2024-02-19 03:45:02 +0300

    add git support

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,7 +6,7 @@ return {
       init = function()
          local builtin = require("telescope.builtin")
          vim.keymap.set("n", "<leader>t", builtin.find_files, {})
-         vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
+         vim.keymap.set("n", "<leader>r", builtin.live_grep, {})
          --vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
          --vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
       end,
