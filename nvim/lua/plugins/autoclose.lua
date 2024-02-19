5898af7f877bfd875467403e2c7eefc37c81b296
Andrey Sidorov <takandar@gmail.com>
2024-02-19 03:16:53 +0300

    temporarily remove plugins

--- nvim/lua/plugins/autoclose.lua
+++ nvim/lua/plugins/autoclose.lua
@@ -1,30 +1,30 @@
 return {
-   {
-      "m4xshen/autoclose.nvim",
-      opts = {
-         options = {
-            disabled_filetypes = { "text" },
-            disable_when_touch = true,
-            pair_spaces = true,
-         },
-         keys = {
-            ["'"] = {
-               escape = true,
-               close = true,
-               pair = "''",
-               disabled_filetypes = { "markdown" },
-            },
-            ["`"] = { escape = false, close = true, pair = "``" },
-            ["<"] = {
-               escape = false,
-               close = true,
-               pair = "<>",
-               enabled_filetypes = { "rust" },
-            },
-         },
-      },
-   },
-   "windwp/nvim-ts-autotag",
+   --{
+   --   "m4xshen/autoclose.nvim",
+   --   opts = {
+   --      options = {
+   --         disabled_filetypes = { "text" },
+   --         disable_when_touch = true,
+   --         pair_spaces = true,
+   --      },
+   --      keys = {
+   --         ["'"] = {
+   --            escape = true,
+   --            close = true,
+   --            pair = "''",
+   --            disabled_filetypes = { "markdown" },
+   --         },
+   --         ["`"] = { escape = false, close = true, pair = "``" },
+   --         ["<"] = {
+   --            escape = false,
+   --            close = true,
+   --            pair = "<>",
+   --            enabled_filetypes = { "rust" },
+   --         },
+   --      },
+   --   },
+   --},
+   --"windwp/nvim-ts-autotag",
    {
       "kylechui/nvim-surround",
       version = "*",
