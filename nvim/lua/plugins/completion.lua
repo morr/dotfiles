9dd0f2694adb6236f8d7c3229483d4744c1828eb
Andrey Sidorov <takandar@gmail.com>
2024-03-04 20:54:01 +0300

    updates to completion plugin

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -8,12 +8,10 @@ return {
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
       "hrsh7th/cmp-buffer",
-      -- "L3MON4D3/LuaSnip",
-      -- "saadparwaiz1/cmp_luasnip",
-      -- "onsails/lspkind.nvim",
+      "onsails/lspkind.nvim",
     },
     config = function()
-      -- local lspkind = require("lspkind")
+      local lspkind = require("lspkind")
       local cmp = require("cmp")
       cmp.setup({
         -- completion = {
@@ -66,20 +64,12 @@ return {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
         },
-        -- mapping = cmp.mapping.preset.insert({
-        -- }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          -- { name = "luasnip" },
           { name = "nvim_lua" },
           { name = "buffer" },
           { name = "path" },
         }),
-        -- snippet = {
-        --    expand = function(args)
-        --       require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-        --    end,
-        -- },
         enabled = function()
           -- disable completion in comments
           local context = require("cmp.config.context")
@@ -92,29 +82,20 @@ return {
               and not context.in_syntax_group("Comment")
           end
         end,
-        -- formatting = {
-        --    format = lspkind.cmp_format({
-        --       mode = "symbol_text",
-        --       maxwidth = 50,
-        --       ellipsis_char = "...",
-        --       menu = {
-        --          buffer = "[Buffer]",
-        --          nvim_lsp = "[LSP]",
-        --          nvim_lua = "[Lua]",
-        --          luasnip = "[LuaSnip]",
-        --          latex_symbols = "[Latex]",
-        --       },
-        --       -- before = function(entry, vim_item)
-        --       --    vim_item = require("tailwindcss-colorizer-cmp").formatter(
-        --       --       entry,
-        --       --       vim_item
-        --       --    )
-        --       --    return vim_item
-        --       -- end,
-        --    }),
-        -- },
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol_text",
+            maxwidth = 50,
+            ellipsis_char = "...",
+            menu = {
+              buffer = "[Buffer]",
+              nvim_lsp = "[LSP]",
+              nvim_lua = "[Lua]",
+            },
+          }),
+        },
       })
-      --
+
       cmp.setup.cmdline({ "/", "?" }, {
         mapping = cmp.mapping.preset.cmdline(),
         sources = {
