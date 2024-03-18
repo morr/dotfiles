3bf44129c80d54e1890faa1a8075a0e785f1d824
Andrey Sidorov <takandar@gmail.com>
2024-03-18 21:17:19 +0300

    add another hotney for code action

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -76,6 +76,7 @@ return {
 
       opts.desc = "See available code actions"
       keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+      keymap.set({ "n", "v" }, ",r", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
       opts.desc = "Smart rename"
       keymap.set("n", "<leader>lR", vim.lsp.buf.rename, opts) -- smart rename
@@ -216,13 +217,13 @@ return {
         keymap.set(
           { "n", "i" },
           "<leader>R",
-          "<cmd>TermExec cmd='cargo run'<cr>",
+          "<cmd>TermExec cmd='MTL_HUD_ENABLED=1 cargo run'<cr>",
           { buffer = bufnr }
         )
         keymap.set(
           { "n", "i" },
           "<leader>r",
-          "<cmd>TermExec cmd='cargo run; exit'<cr>",
+          "<cmd>TermExec cmd='MTL_HUD_ENABLED=1 cargo run; exit'<cr>",
           { buffer = bufnr }
         )
       end,
