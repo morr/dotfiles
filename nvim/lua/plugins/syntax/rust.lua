49473588cf7763b42d2a04d276fbefb58f0a4e69
Andrey Sidorov <takandar@gmail.com>
2024-03-24 20:28:48 +0300

    add cargo run hotkeys

--- nvim/lua/plugins/syntax/rust.lua
+++ nvim/lua/plugins/syntax/rust.lua
@@ -32,6 +32,21 @@ return {
             )
 
             vim.keymap.set("n", "J", "<cmd>RustLsp joinLines<cr>", opts)
+
+            opts.desc = "cargo run"
+            vim.keymap.set(
+              { "n", "i" },
+              "<leader>R",
+              "<cmd>TermExec cmd='MTL_HUD_ENABLED=1 cargo run'<cr>",
+              opts
+            )
+            opts.desc = "cargo run and exit"
+            vim.keymap.set(
+              { "n", "i" },
+              "<leader>r",
+              "<cmd>TermExec cmd='MTL_HUD_ENABLED=1 cargo run; exit'<cr>",
+              opts
+            )
           end,
           -- default_settings = {
           --   -- rust-analyzer language server configuration
