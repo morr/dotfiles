868832200a1e3c554f0be0172828b8310da1717d
Andrey Sidorov <takandar@gmail.com>
2024-04-24 14:26:08 +0300

    fine tune how autocompletion works

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -50,26 +50,30 @@ return {
         --    -- ["<S-Tab>"] = cmp.mapping.select_prev_item(),
         -- },
         mapping = {
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item({
+            behavior = cmp.SelectBehavior.Select,
+          }),
+          ["<C-n>"] = cmp.mapping.select_next_item({
+            behavior = cmp.SelectBehavior.Select,
+          }),
           ["<C-d>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           -- ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.close(),
-          -- ["<CR>"] = cmp.mapping.confirm {
-          --    behavior = cmp.ConfirmBehavior.Replace,
-          --    select = true,
-          -- },
+          ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
-              cmp.select_next_item()
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
             else
               fallback()
             end
           end, { "i", "s" }),
           ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
-              cmp.select_prev_item()
+              cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
             elseif has_words_before() then
               cmp.complete()
             else
