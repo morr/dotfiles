ddf9d5507a578f3e4dc676ec1039bf07b23b76a4
Andrey Sidorov <takandar@gmail.com>
2024-04-28 13:34:48 +0300

    treesitter wgsl

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,14 +4,15 @@ return {
     build = ":TSUpdate",
     config = function()
       vim.filetype.add({ extension = { wgsl = "wgsl" } })
-      -- local parser_config =
-      --   require("nvim-treesitter.parsers").get_parser_configs()
-      -- parser_config.wgsl = {
-      --   install_info = {
-      --     url = "https://github.com/szebniok/tree-sitter-wgsl",
-      --     files = { "src/parser.c" },
-      --   },
-      -- }
+
+      local parser_config =
+        require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.wgsl = {
+        install_info = {
+          url = "https://github.com/szebniok/tree-sitter-wgsl",
+          files = { "src/parser.c" },
+        },
+      }
 
       require("nvim-treesitter.configs").setup({
         auto_install = true,
@@ -45,6 +46,7 @@ return {
           "tsx",
           "vue",
           "yaml",
+          "pug",
           -- other
           "elixir",
           "erlang",
