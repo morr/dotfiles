22af4fe59642e604645dac162d62e7b38f1f5d41
Andrey Sidorov <takandar@gmail.com>
2024-05-13 11:39:30 +0300

    changes to nvim config

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,27 +1,27 @@
 return {
   -- broken in nvim 10 nightly
-  -- {
-  --   "nvim-neo-tree/neo-tree.nvim",
-  --   -- branch = "v3.x",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-  --     "MunifTanjim/nui.nvim",
-  --     "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  --   },
-  --   init = function()
-  --     vim.keymap.set(
-  --       "n",
-  --       "<leader>n",
-  --       "<cmd>Neotree toggle<CR>",
-  --       { desc = "Neotree toggle" }
-  --     )
-  --     vim.keymap.set(
-  --       "n",
-  --       "<leader>N",
-  --       "<cmd>Neotree reveal<CR>",
-  --       { desc = "Neotree reveal" }
-  --     )
-  --   end,
-  -- },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    -- branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    init = function()
+      vim.keymap.set(
+        "n",
+        "<leader>n",
+        "<cmd>Neotree toggle<CR>",
+        { desc = "Neotree toggle" }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>N",
+        "<cmd>Neotree reveal<CR>",
+        { desc = "Neotree reveal" }
+      )
+    end,
+  },
 }
